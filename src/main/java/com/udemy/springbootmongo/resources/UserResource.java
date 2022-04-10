c5cd5968e83544beb164cc463daf92a2798084f9
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-04-10 09:16:51 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/udemy/springbootmongo/resources/UserResource.java
+++ src/main/java/com/udemy/springbootmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.udemy.springbootmongo.domain.Post;
 import com.udemy.springbootmongo.domain.User;
 import com.udemy.springbootmongo.dto.UserDTO;
 import com.udemy.springbootmongo.services.UserService;
@@ -27,6 +28,7 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 	
+	// busca usuários
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {		 
 		List<User> list = userService.findAll();
@@ -63,5 +65,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	// busca postagens
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
