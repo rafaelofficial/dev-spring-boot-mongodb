b67b3a2e367f7a5c1bf3db341d8882c572a343f7
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-04-10 09:24:28 -0300

    Obtendo um post por id

---
+++ src/main/java/com/udemy/springbootmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.udemy.springbootmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.springbootmongo.domain.Post;
+import com.udemy.springbootmongo.repository.PostRepository;
+import com.udemy.springbootmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
