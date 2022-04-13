71ebaa015aa13f255189c08d572cd579d5b964cc
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-04-13 12:44:23 -0300

    Consulta com vários critérios

--- src/main/java/com/udemy/springbootmongo/config/Instantiation.java
+++ src/main/java/com/udemy/springbootmongo/config/Instantiation.java
@@ -39,8 +39,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("2022/03/21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("2022/03/23"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 					
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2022"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2022"), new AuthorDTO(bob));
